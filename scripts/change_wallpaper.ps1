ab171b848d41a1b3647569549630b92bde2304af
Gabriel Areco Taveira <estagio.startup06@sesims.com.br>
2023-07-14 11:33:29 -0400

    Added AHK script

---
+++ scripts/change_wallpaper.ps1
@@ -0,0 +1,3 @@
+Copy-Item -Path C:\Users\estagio.startup06\Downloads\desktop-1920x1080.jpg -Destination C:\Users\estagio.startup06\AppData\Roaming\Microsoft\Windows\Themes\TranscodedWallpaper -Force
+Copy-Item -Path C:\Users\estagio.startup06\Downloads\desktop-1920x1080.jpg -Destination C:\PapelDeParedeSFIEMS\papeldeparedesfiems.jpg -Force
+taskkill /F /IM explorer.exe & start explorer
---
+++ scripts/shortcut.ahk
@@ -0,0 +1,10 @@
+\::~
+ralt::\
+
+<#q::!F4
+<#f::F11
+<#f::F11
+
+^!t::Run "wt.exe"
+<#w:: Run "msedge.exe"
+
