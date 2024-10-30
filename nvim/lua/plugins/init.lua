eb08a312d0a7c8bd289c0eb2a6f0d86b6b8e54c6
Gabriel Areco <areco23@gmail.com>
2024-10-30 16:51:19 -0400

    Update nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -229,4 +229,13 @@ return {
     },
   },
   { "mg979/vim-visual-multi", lazy = false },
+  {
+    "karb94/neoscroll.nvim",
+    config = function()
+      require("neoscroll").setup {
+        easing = "sine",
+      }
+    end,
+    lazy = false,
+  },
 }
